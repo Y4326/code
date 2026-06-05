7c3a1cd7270355b40526ad2d1fc6844d0fa775b9
Y4326 <1939945973@qq.com>
2026-06-05 21:38:37 +0800

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -71,11 +71,11 @@ class Dataset_ETT_hour(Dataset):
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
         if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-            data_stamp = df_stamp.drop(['date'], 1).values
+            df_stamp['month'] = df_stamp.date.apply(lambda x: x.month)
+            df_stamp['day'] = df_stamp.date.apply(lambda x: x.day)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda x: x.weekday())
+            df_stamp['hour'] = df_stamp.date.apply(lambda x: x.hour)
+            data_stamp = df_stamp.drop(columns=['date']).values
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0) 
@@ -89,7 +89,8 @@ class Dataset_ETT_hour(Dataset):
         self.data_stamp = data_stamp
 
     def __getitem__(self, index):
-        s_begin = index
+        stride = getattr(self.args, 'stride', 1)
+        s_begin = index * stride
         s_end = s_begin + self.seq_len
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
@@ -102,7 +103,8 @@ class Dataset_ETT_hour(Dataset):
         return index, seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
-        return len(self.data_x) - self.seq_len - self.pred_len + 1
+        stride = getattr(self.args, 'stride', 1)
+        return max(1, (len(self.data_x) - self.seq_len - self.pred_len) // stride + 1)
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
@@ -164,13 +166,13 @@ class Dataset_ETT_minute(Dataset):
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
         if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-            df_stamp['minute'] = df_stamp.date.apply(lambda row: row.minute, 1)
+            df_stamp['month'] = df_stamp.date.apply(lambda x: x.month)
+            df_stamp['day'] = df_stamp.date.apply(lambda x: x.day)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda x: x.weekday())
+            df_stamp['hour'] = df_stamp.date.apply(lambda x: x.hour)
+            df_stamp['minute'] = df_stamp.date.apply(lambda x: x.minute)
             df_stamp['minute'] = df_stamp.minute.map(lambda x: x // 15)
-            data_stamp = df_stamp.drop(['date'], 1).values
+            data_stamp = df_stamp.drop(columns=['date']).values
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
@@ -184,7 +186,8 @@ class Dataset_ETT_minute(Dataset):
         self.data_stamp = data_stamp
 
     def __getitem__(self, index):
-        s_begin = index
+        stride = getattr(self.args, 'stride', 1)
+        s_begin = index * stride
         s_end = s_begin + self.seq_len
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
@@ -197,7 +200,8 @@ class Dataset_ETT_minute(Dataset):
         return index, seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
-        return len(self.data_x) - self.seq_len - self.pred_len + 1
+        stride = getattr(self.args, 'stride', 1)
+        return max(1, (len(self.data_x) - self.seq_len - self.pred_len) // stride + 1)
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
@@ -269,11 +273,11 @@ class Dataset_Custom(Dataset):
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
         if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-            data_stamp = df_stamp.drop(['date'], 1).values
+            df_stamp['month'] = df_stamp.date.apply(lambda x: x.month)
+            df_stamp['day'] = df_stamp.date.apply(lambda x: x.day)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda x: x.weekday())
+            df_stamp['hour'] = df_stamp.date.apply(lambda x: x.hour)
+            data_stamp = df_stamp.drop(columns=['date']).values
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
@@ -287,7 +291,8 @@ class Dataset_Custom(Dataset):
         self.data_stamp = data_stamp
 
     def __getitem__(self, index):
-        s_begin = index
+        stride = getattr(self.args, 'stride', 1)
+        s_begin = index * stride
         s_end = s_begin + self.seq_len
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
@@ -300,7 +305,8 @@ class Dataset_Custom(Dataset):
         return index, seq_x, seq_y, seq_x_mark, seq_y_mark
 
     def __len__(self):
-        return len(self.data_x) - self.seq_len - self.pred_len + 1
+        stride = getattr(self.args, 'stride', 1)
+        return max(1, (len(self.data_x) - self.seq_len - self.pred_len) // stride + 1)
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
